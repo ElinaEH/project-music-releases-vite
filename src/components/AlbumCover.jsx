1bf91cab75a8e37e5290ed643607b20a21e6b8b6
Elina Eriksson Hult <elina@MacBook-Pro-som-tillhor-Elina.local>
2024-10-12 22:19:37 +0200

    Added content to readme file

--- src/components/AlbumCover.jsx
+++ src/components/AlbumCover.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 const AlbumCover = ({ coverUrl }) => {
   return (
     <div className="album-cover">
--- src/components/ArtistName.jsx
+++ src/components/ArtistName.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 const ArtistName = ({ name, url, isLast }) => {
   return (
     <>
